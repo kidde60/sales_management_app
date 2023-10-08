e9c3e056bfcd0ad3543afb711779fc640f6e4f30
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-08 19:26:25 +0300

    Add Item class

--- lib/screens/item_screen.dart
+++ lib/screens/item_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'item.dart';
+
 class ItemScreen extends StatefulWidget {
   const ItemScreen({super.key});
 
@@ -8,30 +10,32 @@ class ItemScreen extends StatefulWidget {
 }
 
 class _ItemScreenState extends State<ItemScreen> {
+  List<Item> items = List.empty(growable: true);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20.0),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                   hintText: 'Item name',
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)))),
               maxLength: 15,
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                   hintText: 'Quantity',
                   border: OutlineInputBorder(
@@ -39,10 +43,10 @@ class _ItemScreenState extends State<ItemScreen> {
               maxLength: 15,
               keyboardType: TextInputType.number,
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                   hintText: 'Cost Of Item',
                   border: OutlineInputBorder(
@@ -50,19 +54,36 @@ class _ItemScreenState extends State<ItemScreen> {
               maxLength: 15,
               keyboardType: TextInputType.number,
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                   hintText: 'Selling Price',
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10)))),
               maxLength: 15,
               keyboardType: TextInputType.number,
-            )
+            ),
+            items.isEmpty
+                ? const Text('No item add, Please add Items')
+                : ListView.builder(
+                    itemBuilder: ((context, index) => getRow(index))),
           ],
         ),
+      ),
+    );
+  }
+
+  getRow(int index) {
+    return ListTile(
+      title: Column(
+        children: [
+          Text(items[index].name),
+          Text(items[index].quantity.toString()),
+          Text(items[index].cost.toString()),
+          Text(items[index].price.toString()),
+        ],
       ),
     );
   }
