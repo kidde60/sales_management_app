5a646b6b52d8d8b89a056c95fba7a40d983be790
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-19 15:56:26 +0300

    Add signup

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,15 +5,35 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(30.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Image.asset('assets/images/wel.webp'),
+            Image.asset(
+              'assets/images/wel.webp',
+              height: height * 0.6,
+            ),
             const Text(
-                'Welcome to our online sales management system where all sales, profits and losses are managed in a single place.'),
-            OutlinedButton(onPressed: () {}, child: const Text('Login'))
+              'Welcome to our online sales management system where all sales, profits and losses are managed in a single place.',
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: OutlinedButton(
+                        onPressed: () {}, child: Text('Login'.toUpperCase()))),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                    child: ElevatedButton(
+                        onPressed: () {}, child: Text('signup'.toUpperCase())))
+              ],
+            ),
           ],
         ),
       ),
