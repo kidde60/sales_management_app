167284e01ce5950d05e028b7af47a758aadd6109
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-19 08:26:11 +0300

    Add welcome page

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.asset('assets/images/welcome1.avif'),
+          ],
+        ),
+      ),
+    );
+  }
+}
