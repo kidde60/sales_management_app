db12f321a8807dcae625f31ca40f453efda86247
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-20 12:11:26 +0300

    Add google login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,59 +9,85 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/images/wel.webp',
-                height: height * 0.3,
-              ),
-              const Text('Welcome Back'),
-              Form(
-                  child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.person_outline_outlined),
-                          labelText: 'E-Mail',
-                          hintText: 'E-Mail',
-                          border: OutlineInputBorder()),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.lock),
-                          labelText: 'Password',
-                          hintText: 'Password',
-                          border: OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                              onPressed: null,
-                              icon: Icon(Icons.remove_red_eye_sharp))),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Forgot Password?',
-                          )),
-                    ),
-                    SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Login'.toUpperCase())))
-                  ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/wel.webp',
+                  height: height * 0.3,
                 ),
-              ))
-            ],
+                const Text('Welcome Back'),
+                Form(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.person_outline_outlined),
+                            labelText: 'E-Mail',
+                            hintText: 'E-Mail',
+                            border: OutlineInputBorder()),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            labelText: 'Password',
+                            hintText: 'Password',
+                            border: OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.remove_red_eye_sharp))),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forgot Password?',
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Login'.toUpperCase()))),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text('OR'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text('Login with Google'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Text('Don\'t have an account?'),
+                          TextButton(
+                              onPressed: () {},
+                              child: Text('Signup'.toUpperCase()))
+                        ],
+                      )
+                    ],
+                  ),
+                ))
+              ],
+            ),
           ),
         ),
       ),
