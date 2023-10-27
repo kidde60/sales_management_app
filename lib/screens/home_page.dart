b2ec0503d966345b35c1d8eb6bc2dbbee208a419
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-27 11:55:52 +0300

    Add serach bar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,9 +13,23 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LIST OF ITEMS'),
+        leading: const Icon(Icons.menu, color: Colors.black),
+        title: Text('sales management app'.toUpperCase()),
         elevation: 0.0,
         centerTitle: true,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 20, top: 7),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.person_2_outlined),
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
@@ -23,9 +37,203 @@ class _HomePageState extends State<HomePage> {
                 MaterialPageRoute(builder: (context) => const ItemScreen()));
           },
           child: const Icon(Icons.add)),
-      body: const SafeArea(
-          child: Column(
-        children: [Text('Item 1'), Text('Item 2'), Text('Item 3')],
+      body: SingleChildScrollView(
+          child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Hello, Professor'),
+            const Text(
+              'Monitor Your Sales',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            // search bar
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: const BoxDecoration(
+                  border: Border(left: BorderSide(width: 4))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Search ......',
+                    style: TextStyle(
+                        color: Colors.grey.withOpacity(0.5),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Icon(Icons.mic),
+                ],
+              ),
+            ),
+
+            // Categories
+            SizedBox(
+              height: 45,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: 170,
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 45,
+                          width: 45,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black),
+                          child: const Center(
+                            child: Text(
+                              'PV',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'PolyVynle',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                '50 pieces',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 15),
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 170,
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 45,
+                          width: 45,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black),
+                          child: const Center(
+                            child: Text(
+                              'PV',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'PolyVynle',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                '50 pieces',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 15),
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 170,
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 45,
+                          width: 45,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.black),
+                          child: const Center(
+                            child: Text(
+                              'PV',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Flexible(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'PolyVynle',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 16),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                '50 pieces',
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w200,
+                                    fontSize: 15),
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
