ec9b2186830d9db09c943fa47f19bf59f54f00b5
kidde60 <kgeorgewilliam60@gmail.com>
2023-10-28 09:52:26 +0300

    Add constructor

--- lib/reusable/categories_model.dart
+++ lib/reusable/categories_model.dart
@@ -4,7 +4,13 @@ class Categories {
   final String title;
   final String subtitle;
   final String description;
-  final VoidCallback onPress;
+  final VoidCallback? onPress;
 
   Categories(this.title, this.subtitle, this.description, this.onPress);
+
+  static List<Categories> list = [
+    Categories('PV', 'PolyVinyle', '50 pieces', null),
+    Categories('Cot', 'Cotton', '27 pieces', null),
+    Categories('Pol', 'Polyester', '22.5 pieces', null)
+  ];
 }
